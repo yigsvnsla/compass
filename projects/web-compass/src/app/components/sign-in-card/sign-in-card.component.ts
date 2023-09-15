d1835f85884f419830ffb1cf05a2d4b667c012d3
yigs <yigsvnsla@gmail.com>
2023-09-15 09:15:58 -0500

    error autofill form validator not detect changes

--- projects/web-compass/src/app/components/sign-in-card/sign-in-card.component.ts
+++ projects/web-compass/src/app/components/sign-in-card/sign-in-card.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, inject } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { AuthService } from '../../core/services/auth.service';
 import { CommonModule, NgClass } from '@angular/common';
 
@@ -21,22 +21,57 @@ export class SignInCardComponent {
   private formBuilder: FormBuilder = inject(FormBuilder);
   private _authService: AuthService = inject(AuthService);
 
-  public formSignIn: FormGroup = this.formBuilder.nonNullable.group({
-    username: [{ value: 'admin', disabled: false }, [Validators.required]],
-    password: [{ value: 'admin', disabled: false }, [Validators.required]]
+  public formSignIn = this.formBuilder.nonNullable.group({
+    username: [{ value: '', disabled: false }, []],
+    password: [{ value: '', disabled: false }, []]
   });
 
+  ngAfterViewInit(): void {
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
+
+  }
+
+  ngAfterContentInit(): void {
+    //Called after ngOnInit when the component's or directive's content has been initialized.
+    //Add 'implements AfterContentInit' to the class.
+    // console.log('ContentInit', this.formSignIn.getRawValue());
+  }
+
+  public fix($Event: Event) {
+    console.log($Event);
+
+    console.log(this.formSignIn.getRawValue());
+
+  }
+
   public onSubmit(): void {
-    this._authService
-      .singIn(this.formSignIn.value)
-      .subscribe()
+    console.info(this.formSignIn.getRawValue())
+    // this._authService
+    //   .singIn(this.formSignIn.getRawValue())
+    //   .subscribe()
   }
 
-  public get formControlUsername() {
-    return this.formSignIn.get(['username'])
+  public get formControlUsername(): FormControl<string> {
+    return this.formSignIn.controls['username']
   }
 
-  public get formControlPassword() {
-    return this.formSignIn.get(['password'])
+  public get formControlPassword(): FormControl<string> {
+    return this.formSignIn.controls['password']
   }
+
+  public get formControlUsernameStyleState() {
+    return {
+      'input-error': this.formControlUsername.invalid && this.formControlUsername.touched,
+      'input-success': this.formControlUsername.valid && this.formControlUsername.touched
+    }
+  }
+
+  public get formControlPasswordStyleState() {
+    return {
+      'input-error': this.formControlPassword.invalid && this.formControlPassword.touched,
+      'input-success': this.formControlPassword.valid && this.formControlPassword.touched
+    }
+  }
+
 }
